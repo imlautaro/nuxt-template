0afc459f60dea717ee00f31269b8f57ceaf8d175
Lautaro Pereyra <hi@imlautaro.com>
2021-10-08 09:52:59 -0300

    add windicss ui framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api/module'],
+	buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api/module', 'nuxt-windicss'],
 	components: true,
 	srcDir: 'src',
 	target: 'static',
